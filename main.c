8b0ad98ac3592b91fc594f2f82cff1c6131d3bc8
shiliang <shiliangcz@github.com>
2021-11-14 21:13:42 +0800

    添加发包的部分

--- main.c
+++ main.c
@@ -1,14 +1,18 @@
+#include <sys/socket.h>
+//#include <netinet/if_ether.h>
+//#include <net/ethernet.h> //链路层
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <net/if.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
-#include <sys/socket.h>
-
 #include <arpa/inet.h>
+#include <linux/if_ether.h>
+#include <linux/if_packet.h>
 
 
+// socket bind port (recv)
 
 int socket_init()
 {
@@ -16,12 +20,12 @@ int socket_init()
    int sock;
    memset(&ifr, 0x0, sizeof(struct ifreq));
 
-   if((sock = socket(PF_PACKET, SOCK_RAW, htons(0x0003))) < 0)
+   if((sock = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0)
    {
        perror(" socket ");
        exit(0);
    }
-
+   	//设置  eth0 网卡 的 flag
    strcpy(ifr.ifr_name, "eth0");
    if(ioctl(sock, SIOCGIFFLAGS, &ifr) == -1)
    {
@@ -30,7 +34,7 @@ int socket_init()
    }
 
    
-    ifr.ifr_flags |= IFF_PROMISC;
+   ifr.ifr_flags |= IFF_PROMISC;
    if(ioctl(sock, SIOCSIFFLAGS, &ifr) == -1)
    {
        perror("fejwaio?");
@@ -60,27 +64,59 @@ void print_packet(unsigned char *buff,int len)
 
 int main(void)
 {
-	int sock = socket_init();
+	
     unsigned   char	buffer [ 65536 ] ;
-	struct sockaddr in_addr ;
-    socklen_t socklen;
+	struct  sockaddr_ll  in_addr ;
+	struct  sockaddr_ll  dest ;
+	//struct sockaddr in_addr ;
+	int sendfd;
+    int socklen = 0;
 	ssize_t recv_len = 0;
+	int n = 0;
+	struct ifreq ifr;
+	struct packet_mreq mr;
 
 	
-      printf("Now I'm going to make a bridge for vm taps\n");
+    printf("Now I'm going to make a bridge for vm taps\n");
 
-	  // socket bind port (recv)
+
+    int sock = socket_init();
+	socklen = sizeof(struct  sockaddr_ll);
+    sendfd = socket(PF_PACKET,SOCK_RAW,htons(ETH_P_ALL) ) ;
+    strcpy ( ifr.ifr_name , "eth0" ) ;
+   	//获取eth0 网卡 的 mac
+    ioctl ( sendfd , SIOCGIFINDEX , &ifr ) ;
+    mr.mr_ifindex = ifr.ifr_ifindex ;
+    mr.mr_type = PACKET_MR_PROMISC ;
+    if ( setsockopt (sendfd , SOL_PACKET , PACKET_ADD_MEMBERSHIP, &mr,sizeof(mr))== -1)
+    {
+      perror ( " can not set to promisc\n" ) ;
+      exit ( 0 ) ;
+    }
    
-	 while(1)
-     {
-       
-       memset(&in_addr, 0x0, socklen);
-       recv_len = recvfrom(sock, buffer, sizeof(buffer), 0,
-           (struct sockaddr *)&in_addr, &socklen); 
-	   print_packet(buffer,20);
+    while(1)
+    {
       
-     }
-	 
-     return 0;
+      memset(&in_addr, 0x0, socklen);
+	  memset(&dest, 0x0, socklen);
+      recv_len = recvfrom(sock, buffer, sizeof(buffer), 0,
+          (struct sockaddr *)&in_addr, &socklen); 
+      print_packet(buffer,34);
+     
+      if(in_addr.sll_pkttype == PACKET_OUTGOING || in_addr.sll_pkttype ==PACKET_HOST) 
+	  {
+	      printf("won't send me again;\n");
+		  continue;
+	  } 
+      dest.sll_family=AF_PACKET;
+      dest.sll_ifindex =ifr.ifr_ifindex;
+      dest.sll_halen=htons(ETH_HLEN); 
+      memmove(&dest.sll_addr,buffer,sizeof(buffer[0])*8);
+      n=sendto(sendfd,buffer,recv_len,0,(struct sockaddr*)&dest,sizeof(dest));
+      printf(" send n=%d\n",n);
+     
+    }
+    
+    return 0;
 
 }
